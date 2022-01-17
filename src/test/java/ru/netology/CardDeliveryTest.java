7098f8e000927373e7ec533942948a1f363c0224
Sergey Obidin <player181285@gmail.com>
2022-01-17 22:29:38 +0400

    added new tast for task 2

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -34,6 +34,24 @@ public class CardDeliveryTest {
     }
 
 
-
+    @Test
+    public void shouldSendTrueFullFormWithPopUpLineAndCalendar()   {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Са");
+        $(withText("Саратов")).click();
+        String date = LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(date);
+        $(".calendar").sendKeys(Keys.ARROW_DOWN, Keys.ENTER);
+        $("[data-test-id='name'] input").setValue("Малыш Барбоскин");
+        $(byName("phone")).setValue("+77777777777"); // просто что бы попробовать поиск по имени
+        $("[data-test-id='agreement']").click();
+        $(withText("Забронировать")).click();
+        String dateOfMeetingExpected = LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id='notification']").shouldBe(exactText("Успешно! " +
+                "Встреча успешно забронирована на "
+                + dateOfMeetingExpected), Duration.ofSeconds(15));
+    }
 
 }
